e6111b8cbd6eeda69d5e02322f05d5ba9b9e1648
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2019-06-02 15:27:43 +0530

    login set up added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResumeFormComponent } from './resume-form/resume-form.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   { path: 'resume-form', component: ResumeFormComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule }   from '@angular/forms';
+import { CustomMaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { ResumeFormComponent } from './resume-form/resume-form.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResumeFormComponent
+    ResumeFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
+  exports: [CustomMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
